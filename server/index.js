eb22bd2cd06a6861e15944482b580a44c3eb1df8
Tunmise Ogunniyi <ogunniyitunmise@gmail.com>
2018-09-16 11:11:49 +0100

    Create a basic react app

--- server/index.js
+++ server/index.js
@@ -1,12 +1,10 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
-
 const port = process.env.PORT || 8000;
 
-app.get('/', (req, res) => {
-  res.send('Welcom to nothing');
-});
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.listen(port, () => {
   console.log(`App started on ${port}...`);
